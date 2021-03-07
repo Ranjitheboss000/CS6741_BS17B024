7fc05f0a93782318101c4d66ea6a36155a57b9b1
Ranjith Kumar R M <39576830+Ranjitheboss000@users.noreply.github.com>
2021-03-07 11:42:21 +0530

    Add files via upload

---
+++ Assignment 2/Problem 2.jl
@@ -0,0 +1,58 @@
+using DataFrames,Dates,PrettyTables
+
+untidyData = DataFrame(
+    id = repeat(["MX17004"],outer = 10),
+    year = repeat(["2010"],outer = 10),
+    month = repeat([1,2,3,4,5],inner = 2),
+    element = repeat(["tmax","tmin"],inner = 5),
+    d1 = missings(10),
+    d2 = [missing,missing,27.3,14.4,missing,missing,missing,missing,29,14.6],
+    d3 = [missing,missing,27,14.6,missing,missing,28.8,14.2,missing,missing],
+    d4 = [30,16,missing,missing,27,14.6,missing,missing,29,14.6],
+    d5 = [24,13,26,15,missing,missing,missing,missing,missing,missing],
+    d6 = [missing,missing,missing,missing,missing,missing,23.5,15,28,18],
+    d7 = [missing,missing,missing,missing,27,16,26,13.9,missing,missing],
+    d8 = missings(10),
+    d9 = [missing,missing,27.3,14.4,missing,missing,missing,missing,29,14.6],
+    d10 = [missing,missing,27,14.6,missing,missing,28.8,14.2,missing,missing],
+    d11 = [30,16,missing,missing,27,14.6,missing,missing,29,14.6],
+    d12 = [24,13,26,15,missing,missing,missing,missing,missing,missing],
+    d13 = [missing,missing,missing,missing,missing,missing,23.5,15,28,18],
+    d14 = [missing,missing,missing,missing,27,16,26,13.9,missing,missing],
+    d15 = missings(10),
+    d16 = [missing,missing,27.3,14.4,missing,missing,missing,missing,29,14.6],
+    d17 = [missing,missing,27,14.6,missing,missing,28.8,14.2,missing,missing],
+    d18 = [30,16,missing,missing,27,14.6,missing,missing,29,14.6],
+    d19 = [24,13,26,15,missing,missing,missing,missing,missing,missing],
+    d20 = [missing,missing,missing,missing,missing,missing,23.5,15,28,18],
+    d21 = [missing,missing,missing,missing,27,16,26,13.9,missing,missing],
+    d22 = [missing,missing,27.3,14.4,missing,missing,missing,missing,29,14.6],
+    d23 = [missing,missing,27,14.6,missing,missing,28.8,14.2,missing,missing],
+    d24 = [30,16,missing,missing,27,14.6,missing,missing,29,14.6],
+    d25 = [24,13,26,15,missing,missing,missing,missing,missing,missing],
+    d26 = [missing,missing,missing,missing,missing,missing,23.5,15,28,18],
+    d27 = [missing,missing,missing,missing,27,16,26,13.9,missing,missing],
+    d28 = [30,16,missing,missing,27,14.6,missing,missing,29,14.6],
+    d29 = [24,13,missing,missing,26.3,14.4,missing,missing,missing,missing],
+    d30 = [missing,missing,missing,missing,missing,missing,missing,missing,missing,missing],
+    d31 = [missing,missing,missing,missing,missing,missing,missing,missing,missing,missing],
+
+);
+
+show(untidyData,allrows=true)
+
+days = names(untidyData)[5:35];
+dayD = Dict(d => untidyData[:,d].!== missing  for d in days);
+
+tidyData = DataFrame(id = String[],date = Date[],tmax = Float64[],tmin = Float64[]);
+
+for r = 1:2:9
+    for d = 1:31
+        if dayD[days[d]][r]
+            push!(tidyData,("MX17004",Date(2010,(r+1)/2,d),untidyData[r,days[d]],untidyData[r+1,days[d]]))
+        end
+    end
+end
+
+
+pretty_table(tidyData)
---
+++ Assignment 2/Problem 3.jl
@@ -0,0 +1,49 @@
+# Please install the following packages if not already installed
+
+# using Pkg
+# Pkg.add("DataFrames")
+# Pkg.add("PrettyTables")
+# Pkg.add("CSV")
+# Pkg.add("Dates")
+
+using DataFrames,CSV,Dates,PrettyTables
+
+# Please provide appropriate input argument for CSV.file()
+untidyData = DataFrame(CSV.File("billboard.csv"));
+untidyData = sort!(select(untidyData, Not(["date.peaked","genre"])));
+
+moltenData = DataFrame(year = Int[],artist = String[], time = Time[];track =String[],date = Date[], week = Int[],rank = String[]);
+for i = 1:317
+    for w in 1:76
+        if untidyData[i,w+5] !== "NA" && untidyData[i,w+5] !== missing
+            d = untidyData[i,"date.entered"] + Dates.Day(7*(w-1))
+            push!(moltenData,(2000,untidyData[i,"artist.inverted"],untidyData[i,:time],untidyData[i,:track],d,w,string(untidyData[i,w+5])))
+        end
+    end
+end
+
+show(moltenData, allcols=true,show_row_number = false)
+
+song = select(moltenData,:artist,:track,:time);
+
+insertcols!(song,       
+    1,                # insert as column 1
+    :id => 1:size(song)[1],   # populate as "id" with 1,2,3,..
+    makeunique=true);
+
+show(song, allcols=true,show_row_number = false)
+
+id =[]
+n = 0
+for w in moltenData[:,:week]
+    if w == 1
+        n += 1
+    end
+    push!(id,n)   
+end
+
+rank = select(moltenData,:date,:rank);
+insertcols!(rank,1,:id => id,makeunique=true);
+rank[!,:id] = convert.(Int,rank[!,:id]);
+
+show(rank,show_row_number = false)
